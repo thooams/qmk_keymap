3d106df98adaa257ae5f017141ffcc35695640f8
Thomas HUMMEL <thomas@hummel.link>
2024-10-09 09:52:51 +0200

    core(planck): Update planck rules.mk

--- keyboards/planck/keymaps/thooams/rules.mk
+++ keyboards/planck/keymaps/thooams/rules.mk
@@ -18,3 +18,15 @@ SRC += features/oneshots.c
 SRC += features/overrides.c
 SRC += features/remove_mods.c
 SRC += features/swappers.c
+
+# Planck defaut config
+BOOTMAGIC_ENABLE = yes      # Enable Bootmagic Lite
+MOUSEKEY_ENABLE = no       # Mouse keys
+EXTRAKEY_ENABLE = yes       # Audio control and System control
+CONSOLE_ENABLE = yes         # Console for debug
+COMMAND_ENABLE = no        # Commands for debug and configuration
+NKRO_ENABLE = yes           # Enable N-Key Rollover
+BACKLIGHT_ENABLE = no      # Enable keyboard backlight functionality
+AUDIO_ENABLE = no           # Audio output
+RGBLIGHT_ENABLE = no        # Enable WS2812 RGB underlight.
+LAYOUTS_HAS_RGB = no
