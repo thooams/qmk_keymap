e2a9334d080dc08753c4aba2d8a684407a471144
Thomas HUMMEL <thomas@hummel.link>
2025-10-06 23:01:59 +0200

    Add home row

--- keyboards/planck/keymaps/thooams/keymap.c
+++ keyboards/planck/keymaps/thooams/keymap.c
@@ -6,10 +6,10 @@
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 [DEF] = LAYOUT(
-    KF_Y,       OSL(SPC),   KF_O,       KF_F,       KF_K,       _______,    _______,    KF_V,       KF_P,       KF_L,       KF_C,       KF_Q,
-    KF_I,       KF_A,       KF_E,       KF_U,       KF_COMM,    _______,    _______,    KF_G,       KF_T,       KF_N,       KF_S,       KF_R,
-    KF_J,       KF_DOT,     KF_MINS,    KF_H,       KF_Z,       _______,    _______,    KF_B,       KF_D,       KF_M,       KF_W,       KF_X,
-    _______,    _______,    _______,    MO(NAV),    KC_SPC,     _______,    _______,    KC_RSFT,    MO(SYM),    _______,    _______,    _______,
+    KF_X,     OSL(SPC),            KF_O,                KF_F,                KF_K,     _______,  _______,  KF_Q,     KF_D,                KF_L,                KF_C,                KF_W,
+    KF_A,     MT(MOD_LALT, KF_I),  MT(MOD_LGUI, KF_E),  MT(MOD_LCTL, KF_U),  KF_COMM,  _______,  _______,  KF_G,     MT(MOD_RCTL, KF_T),  MT(MOD_RGUI, KF_N),  MT(MOD_RALT, KF_S),  KF_R,
+    KF_Z,     KF_Y,                KF_MINS,             KF_H,                KF_DOT,   _______,  _______,  KF_B,     KF_P                 KF_M,                KF_V,                KF_J,
+    _______,  _______,             _______,             MO(NAV),             KC_SPC,   _______,  _______,  KC_RSFT,  MO(SYM),             _______,             _______,             _______,
 ),
 
 [SPC] = LAYOUT(
